601ab1ce49246e20abffa0f89437fb6d8e1c1a03
AHUser17 <gherbig@outlook.com>
2023-06-14 22:58:29 -0400

    task 3

--- final_project/router/general.js
+++ final_project/router/general.js
@@ -28,7 +28,19 @@ public_users.get('/isbn/:isbn',function (req, res) {
 // Get book details based on author
 public_users.get('/author/:author',function (req, res) {
   //Write your code here
-  //FUCK YOU
+  const author = req.params.author; //la variable author es el parametro author del request
+  const bookKeys = Object.keys(books);//la variable bookKeys toma los valores de las kes dentro de books
+  let booksByAuthor = [];//declaramos la variable booksByAuthor que es una lista vacia
+  let key;//declaramos la variable key
+
+  for (let i = 0;i<bookKeys.length;i++) { //hacemos un for donde i inicia en el valor 0 y va aumentando de a uno siempre y cuando el valor sea inferior al largo de las key de la lista books (bookKeys)
+    key = bookKeys[i]; //key adquiere su valor, en esta variable ponemos el valor de la ke que esta dentro de la lista
+    if (books[key].author === author) { //utilizamos la key para acceder al libro y tomar el dato author. si el autor de ese libre  es igual a la variable author...
+        booksByAuthor.push(books[key]); //agregamos ese elemento a la lista antes vacia de booksbyauthor
+    }
+  }
+
+  res.send(booksByAuthor) //retornamos el valor de booksbyauthor luego de finalizado el for
   //return res.status(300).json({message: "Yet to be implemented"});
 });
 
